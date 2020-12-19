7991b65c331cb15448877f2c9e1c32a380045539
Ivan Smirnov <i.s.smirnov@gmail.com>
2020-12-19 22:21:59 +0000

    Force recythonizing randomkit

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,3 @@
 make clean
+del /f lightning\impl\randomkit\random_fast.cpp
 %PYTHON% -m pip install . --no-deps -vv
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 make clean
+rm -fv lightning/impl/randomkit/random_fast.cpp
 $PYTHON -m pip install . --no-deps -vv
