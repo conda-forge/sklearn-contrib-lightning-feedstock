bce57c15b600e797949f0b3a0cd2cd223565afe5
Marcel Bargull <marcel.bargull@udo.edu>
2020-12-20 21:07:56 +0100

    Remove egg-info

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,4 +2,6 @@
 for /f "delims=" %%f in ('dir /s /b lightning') do if "%%~xf"==".cpp" del "%%f"
 :: Remove PKG-INFO to trigger cythonize
 del PKG-INFO
+:: Remove egg-info because SOURCES.txt contains hard-coded path from dev machine
+rd /s /q sklearn_contrib_lightning.egg-info
 %PYTHON% -m pip install . --no-deps -vv
--- recipe/build.sh
+++ recipe/build.sh
@@ -3,4 +3,6 @@
 find lightning -name \*.cpp -delete
 # Remove PKG-INFO to trigger cythonize
 rm PKG-INFO
+# Remove egg-info because SOURCES.txt contains hard-coded path
+rm -r sklearn_contrib_lightning.egg-info
 $PYTHON -m pip install . --no-deps -vv
